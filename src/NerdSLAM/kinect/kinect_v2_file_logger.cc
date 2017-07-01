e6cf952d6264d29f239f47950431514094fbd40e
kiddos <kiddo831007@gmail.com>
2017-07-01 15:03:24 +0800

    add kinect v2 file logger

---
+++ src/NerdSLAM/kinect/kinect_v2_file_logger.cc
@@ -0,0 +1,26 @@
+#include "NerdSLAM/kinect/kinect_v2_file_logger.h"
+#include <chrono>
+#include <sstream>
+
+namespace nerd {
+namespace slam {
+
+KinectV2FileLogger::KinectV2FileLogger(const std::string& filepath,
+                                       libfreenect2::Logger::Level level) {
+  if (filepath.length() == 0) {
+    std::stringstream ss;
+    ss << "/tmp/NerdSLAM_kinect_"
+       << std::chrono::high_resolution_clock::now().time_since_epoch().count()
+       << ".log";
+    logfile_.open(ss.str(), std::ios::out);
+  }
+}
+
+void KinectV2FileLogger::log(libfreenect2::Logger::Level level,
+                             const std::string& message) {
+  logfile_ << "[" << libfreenect2::Logger::level2str(level) << "] " << message
+           << std::endl;
+}
+
+} /* end of slam namespace */
+} /* end of nerd namespace */
---
+++ src/NerdSLAM/kinect/kinect_v2_file_logger.h
@@ -0,0 +1,28 @@
+#ifndef KINECT_V2_FILE_LOGGER_H
+#define KINECT_V2_FILE_LOGGER_H
+
+#include <libfreenect2/logger.h>
+#include <fstream>
+#include <string>
+
+namespace nerd {
+namespace slam {
+
+class KinectV2FileLogger : libfreenect2::Logger {
+ public:
+  KinectV2FileLogger(const std::string& filepath,
+                     libfreenect2::Logger::Level level);
+
+  bool ok() { return logfile_.is_open() && logfile_.good(); }
+
+  virtual void log(libfreenect2::Logger::Level level,
+                   const std::string& message);
+
+ private:
+  std::ofstream logfile_;
+};
+
+} /* end of slam namespace */
+} /* end of nerd namespace */
+
+#endif /* end of include guard: KINECT_V2_FILE_LOGGER_H */
