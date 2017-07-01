38faa6fd9e9dc2cf3863faa8f5cbfff44a1785b3
kiddos <kiddo831007@gmail.com>
2017-07-01 14:42:28 +0800

    remove duplicated and rename

--- src/NerdSLAM/kinect/kinect_reader.h
+++ src/NerdSLAM/kinect/kinect_reader.h
@@ -16,14 +16,15 @@ class KinectReader {
   virtual void PauseDevice() = 0;
   virtual void StopDevice() = 0;
 
-  void set_frame_hander(boost::function<void(Frame)> handler) {
+  void set_frame_hander(boost::function<void(FrameMap)> handler) {
     frame_handler_ = std::move(handler);
   }
+  bool is_running() { return running_; }
 
  protected:
   bool running_;
-  boost::function<void(const Frame&)> frame_handler_;
-  Frame frame_;
+  boost::function<void(const FrameMap&)> frame_handler_;
+  FrameMap frame_;
 };
 
 } /* end of slam namespace */
