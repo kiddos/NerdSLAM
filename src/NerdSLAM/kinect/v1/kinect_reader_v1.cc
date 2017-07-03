87edfc259d486520d59a17352a25393eb7cc63e7
kiddos <kiddo831007@gmail.com>
2017-07-04 02:43:55 +0800

    add kinect reader v1 run

--- src/NerdSLAM/kinect/v1/kinect_reader_v1.cc
+++ src/NerdSLAM/kinect/v1/kinect_reader_v1.cc
@@ -1,14 +1,16 @@
 #include "NerdSLAM/kinect/v1/kinect_reader_v1.h"
+#include <chrono>
+#include <thread>
 
 namespace nerd {
 namespace slam {
 namespace v1 {
 
 KinectReaderV1::KinectReaderV1(const KinectConfigV1& config)
-    : config_(config), device_(nullptr) {}
+    : config_(config), device_(nullptr), running_(false) {}
 
 KinectReaderV1::~KinectReaderV1() {
-  if (device_) delete device_;
+  // if (device_) delete device_;
 }
 
 bool KinectReaderV1::FindDevice() {
@@ -19,22 +21,71 @@ bool KinectReaderV1::FindDevice() {
   return true;
 }
 
-void KinectReaderV1::StartDevice() {
-  if (device_) {
-    device_->startVideo();
-    device_->startDepth();
+void KinectReaderV1::Run() {
+  StartDevice();
+  running_ = true;
+  size_t data_size = FRAME_WIDTH * FRAME_HEIGHT * FRAME_CHANNEL;
+  while (running_) {
+    if (!pause_) {
+      if (config_.rgb()) {
+        device_->GetRGB(rgb_);
+        assert(rgb_.size() == data_size);
+        Frame* rgb = new Frame;
+        rgb->set_width(FRAME_WIDTH);
+        rgb->set_height(FRAME_HEIGHT);
+        rgb->set_data(reinterpret_cast<const char*>(&rgb_[0]), data_size);
+        rgb->set_type(Frame::RGB);
+        frames_.set_allocated_color_frame(rgb);
+      }
+      if (config_.depth()) {
+        device_->GetDepth(depth_);
+        assert(rgb_.size() == FRAME_WIDTH * FRAME_HEIGHT * FRAME_CHANNEL);
+        Frame* depth = new Frame;
+        depth->set_width(FRAME_WIDTH);
+        depth->set_height(FRAME_HEIGHT);
+        depth->set_data(reinterpret_cast<const char*>(&depth_[0]), data_size);
+        depth->set_type(Frame::RGB);
+        frames_.set_allocated_depth_frame(depth);
+      }
+    }
+
+    if (frame_handler_) {
+      frame_handler_(frames_);
+    }
+    if (config_.timeout() > 0) {
+      std::this_thread::sleep_for(
+          std::chrono::milliseconds(config_.timeout()));
+    }
   }
 }
 
-void KinectReaderV1::PauseDevice() {
+void KinectReaderV1::StartDevice() {
   if (device_) {
-    device_->stopVideo();
-    device_->stopDepth();
+    if (config_.rgb()) {
+      device_->startVideo();
+    }
+    if (config_.depth()) {
+      device_->startDepth();
+    }
+  }
+  pause_ = false;
+}
+
+void KinectReaderV1::PauseDevice() {
+  pause_ = true;
+  if (device_) {
+    if (config_.rgb()) {
+      device_->stopVideo();
+    }
+    if (config_.depth()) {
+      device_->stopDepth();
+    }
   }
 }
 
 void KinectReaderV1::StopDevice() {
   PauseDevice();
+  running_ = false;
 }
 
 } /* end of v1 namespace */
--- src/NerdSLAM/kinect/v1/kinect_reader_v1.h
+++ src/NerdSLAM/kinect/v1/kinect_reader_v1.h
@@ -12,17 +12,25 @@ namespace v1 {
 
 class KinectReaderV1 : public KinectReader {
  public:
+  enum { FRAME_WIDTH = 640, FRAME_HEIGHT = 480, FRAME_CHANNEL = 3 };
   KinectReaderV1(const KinectConfigV1& config);
   virtual ~KinectReaderV1();
   bool FindDevice() override;
+  void Run() override;
   void StartDevice() override;
   void PauseDevice() override;
   void StopDevice() override;
 
  private:
+  void Loop();
+
   KinectConfigV1 config_;
   Freenect::Freenect freenect_;
   KinectDevice* device_;
+  bool running_, pause_;
+
+  FrameMap frames_;
+  std::vector<uint8_t> rgb_, depth_;
 };
 
 } /* end of v1 namespace */
